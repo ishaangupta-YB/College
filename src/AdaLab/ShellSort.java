0a795f094d2ad9b5bdc8fe78981ffd9745e4be74
Ishaan Gupta <ishaangupta999@gmail.com>
2021-12-14 14:53:41 +0530

    Ada Lab Updated

--- src/AdaLab/ShellSort.java
+++ src/AdaLab/ShellSort.java
@@ -4,33 +4,36 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ShellSort {
- 
-	void shellSort(int a[], int n) {
-		
-		for (int j = n/2;j>0;j/=2) {
-			for (int i=j;i<n;i+=1) {
-				int tmp = a[i];
-				int k=0;
-				for (k=i; k>=j && a[k-j]>tmp;k-=j)
-					a[k]=a[k-j];
-				a[k]=tmp;
-			}
-		}
+	public static void main(String[] args) {
+		Car c1 = new Car("Red");
+		Car c2 = new Car("Green");
+		Car c3 = new Car("Blue");
+		System.out.println("Number of cars created: " + Car.getNumberOfCars());
 	}
- 
-	public static void main(String args[]) {
-		ShellSort s = new ShellSort();
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter size");
-		int n = sc.nextInt();
-		int[] a = new int[n];
-		
-		for(int i=0;i<n;i++) a[i] = sc.nextInt();
-		
-		s.shellSort(a, a.length);
-		
-		
-		System.out.println("Sorted Array"+Arrays.toString(a)); 
+
+	static {
+		System.out.println("Tester class loaded");
+	}
+}
+
+class Car {
+	private static String color;
+	private static int numberOfCars = 0;
+
+	static {
+		System.out.println("Car class loaded");
+	}
+
+	public Car(String c) {
+		color = c;
+		numberOfCars++;
+	}
+
+	public static String getColor() {
+		return color;
+	}
+
+	public static int getNumberOfCars() {
+		return numberOfCars;
 	}
 }
--- src/AdaLab/lab10.java
+++ src/AdaLab/lab10.java
@@ -2,7 +2,7 @@ package AdaLab;
 
 import java.util.*;
 
-class RadixSort {
+public class lab10 {
       static int getMax(int arr[], int n) {
             int mx = arr[0];
             for (int i = 1; i < n; i++)
@@ -34,10 +34,48 @@ class RadixSort {
                   countSort(arr, n, exp);
       }
 
+      static void shellSort(int a[], int n) {
+
+            for (int j = n/2;j>0;j/=2) {
+                  for (int i=j;i<n;i+=1) {
+                        int tmp = a[i];
+                        int k=0;
+                        for (k=i; k>=j && a[k-j]>tmp;k-=j)
+                              a[k]=a[k-j];
+                        a[k]=tmp;
+                  }
+            }
+      }
+
+      static int[] bucketSort(int[] sequence, int maxValue) {
+            int[] Bucket = new int[maxValue + 1];
+            int[] sorted_sequence = new int[sequence.length];
+            for (int i = 0; i < sequence.length; i++)
+                  Bucket[sequence[i]]++;
+            int outPos = 0;
+            for (int i = 0; i < Bucket.length; i++)
+                  for (int j = 0; j < Bucket[i]; j++)
+                        sorted_sequence[outPos++] = i;
+            return sorted_sequence;
+      }
+
+      static int maxValue(int[] sequence) {
+            int maxValue = 0;
+            for (int i = 0; i < sequence.length; i++)
+                  if (sequence[i] > maxValue)
+                        maxValue = sequence[i];
+            return maxValue;
+      }
+
       static void print(int arr[], int n) {
             for (int i = 0; i < n; i++)
                   System.out.print(arr[i] + " ");
             System.out.println();
+      }
+
+      static void printSequence(int[] sorted_sequence) {
+            for (int i = 0; i < sorted_sequence.length; i++)
+                  System.out.print(sorted_sequence[i] + " ");
       }
 
       public static void main(String[] args) {
@@ -46,7 +84,26 @@ class RadixSort {
             System.out.println("Array before sorting");
             print(arr, n);
             radixsort(arr, n);
-            System.out.println("Array after sorting");
+            System.out.println("Array after sorting using Radix Sort");
             print(arr, n);
+
+            int a[] = {2, 3, 1, 0, 9};
+            n = a.length;
+            System.out.println("Array before sorting");
+            print(a, n);
+            System.out.println("Array after sorting using Shell Sort");
+            shellSort(a, n);
+            print(arr,n);
+
+            Random random = new Random();
+            int N = 20;
+            int[] sequence = new int[N];
+            for (int i = 0; i < N; i++)
+                  sequence[i] = Math.abs(random.nextInt(100));
+            int maxValue = maxValue(sequence);
+            System.out.println("\nOriginal Sequence: ");
+            printSequence(sequence);
+            System.out.println("\nArray after sorting using Bucket Sort ");
+            printSequence(bucketSort(sequence, maxValue));
       }
 }
