bd7673805686d3230f386c46c115afc07abaa4e6
Ishaan Gupta <ishaangupta999@gmail.com>
2021-12-19 17:29:02 +0530

    Java Lab Updated

--- src/JavaLab/lab14.java
+++ src/JavaLab/lab14.java
@@ -7,31 +7,32 @@ import java.awt.event.ActionListener;
 public class lab14 {
       lab14(){
             Frame f = new Frame("lab14");
-            f.setSize(700,700);
+            f.setSize(400,400);
 
             TextField tf = new TextField();
-            tf.setBounds(200,300,150,20);
+            tf.setBounds(100,100,150,20);
             f.add(tf);
 
-            Button b1 = new Button("Button 1");
-            b1.setBounds(200,400,150,20);
+            Button b1 = new Button("Clear Textfield");
+            b1.setBounds(100,200,200,20);
             f.add(b1);
 
-            Button b2 = new Button("Button 2");
-            b2.setBounds(400,400,150,20);
+            Button b2 = new Button("Print Textfield");
+            b2.setBounds(100,300,200,20);
             f.add(b2);
 
             b1.addActionListener(new ActionListener() {
                   @Override
                   public void actionPerformed(ActionEvent e) {
-                        System.out.println("Button1 Clicked");
+                        tf.setText(null);
+                        System.out.println("Clear TextField Button Clicked");
                   }
             });
 
             b2.addActionListener(new ActionListener() {
                   @Override
                   public void actionPerformed(ActionEvent e) {
-                        System.out.println("Button2 Clicked\nText in TextField is :"+tf.getText());
+                        System.out.println("Print Textfield Button Clicked\nText in TextField is : "+tf.getText());
                   }
             });
             f.setLayout(null);
